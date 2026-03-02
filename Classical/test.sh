122c823b0a67c8e8aca410961201215f92a9d7a9
Jean-Marc Petit <jean-marc.petit@normalesup.org>
2026-03-02 14:28:56 +0100

    Added clean-up in test. 2026-03-02T13:29 UT.

--- Classical/test.sh
+++ Classical/test.sh
@@ -24,6 +24,7 @@ tail --lines=+3 ModelUsed-check-8.66.dat > zzz2
 diff zzz1 zzz2 > /dev/null 2>&1
 status=$?
 \rm -f zzz1 zzz2
+\rm -f OSSOS-model.out ModelUsed.dat
 if [ $status != 0 ]; then
     echo "================================================================="
     echo "=======================  Test FAILED!   ========================="
